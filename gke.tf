8aaee58b23c9a4e42796e8993a6ad3d1b32db1b3
cx-matts <matt.slotten@checkmarx.com>
2022-12-08 10:02:02 -0500

    Adding a Terraform file and some NPM packages

---
+++ gke.tf
@@ -0,0 +1,43 @@
+data "google_compute_zones" "available_zones" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_container_cluster" "workload_cluster" {
+  pod_security_policy_config {
+		enabled = true
+	}
+
+  name               = "terragoat-${var.environment}-cluster"
+  logging_service    = "none"
+  location           = var.region
+  initial_node_count = 1
+
+  enable_legacy_abac       = false
+  monitoring_service       = "none"
+  remove_default_node_pool = true
+  network                  = google_compute_network.vpc.name
+  subnetwork               = google_compute_subnetwork.public-subnetwork.name
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block = "0.0.0.0/0"
+    }
+  }
+}
+
+resource "google_container_node_pool" "custom_node_pool" {
+  management {
+		auto_repair = true
+	}
+
+  management {
+		auto_upgrade = true
+	}
+
+  cluster  = google_container_cluster.workload_cluster.name
+  location = var.region
+
+  node_config {
+    image_type = "Ubuntu"
+  }
+}
